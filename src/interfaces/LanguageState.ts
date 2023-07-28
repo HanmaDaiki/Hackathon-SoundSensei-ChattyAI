1af0ec0dfec7547b080257d557dc3a82d3cc91ee
Arkady Schapov <sigma696@gmail.com>
2023-07-28 19:48:54 +0300

    feat: add hints translation

--- src/interfaces/LanguageState.ts
+++ src/interfaces/LanguageState.ts
@@ -1,24 +1,28 @@
 export interface lang {
-    storyteller: string;
-    chat: string;
-    library: string;
-    favorites: string;
-    helloMessage: string;
-    inputPlaceholder: string;
-    inputPlaceholderAwaiting: string;
-    keyWords: string;
-    miniStory: string;
-    emptyLib: string;
-    emptyFav: string;
-};
+  storyteller: string;
+  chat: string;
+  library: string;
+  favorites: string;
+  helloMessage: string;
+  inputPlaceholder: string;
+  inputPlaceholderAwaiting: string;
+  keyWords: string;
+  miniStory: string;
+  emptyLib: string;
+  emptyFav: string;
+  hint: string;
+  hintNewTale: string;
+  hintFavTale: string;
+  hintMic: string;
+}
 
 export interface LanguageState {
-    currentLanguage: 'ru' | 'en' | 'zh' | 'ae' | 'fr';
-    language: {
-      ru: lang;
-      en: lang;
-      zh: lang;
-      ae: lang;
-      fr: lang;
-    };
+  currentLanguage: "ru" | "en" | "zh" | "ae" | "fr";
+  language: {
+    ru: lang;
+    en: lang;
+    zh: lang;
+    ae: lang;
+    fr: lang;
+  };
 }
